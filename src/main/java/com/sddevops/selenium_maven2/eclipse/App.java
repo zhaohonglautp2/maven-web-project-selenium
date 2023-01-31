6026b4d86c45abc0c4c78fd811d19076a05f858c
zhaohonglau <lau_zhao_hong@myrp.edu.sg>
2023-01-31 15:05:13 +0800

    initial

---
+++ src/main/java/com/sddevops/selenium_maven2/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.selenium_maven2.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
